badf2648800c920154acf511629c359d31100109
ahua3321 <971342969@qq.com>
2025-01-09 10:53:28 +0800

    Update ipj.py

--- ipj.py
+++ ipj.py
@@ -143,7 +143,8 @@ for url in valid_urls:
                         name = name.replace("CCTV5+体育赛视", "CCTV5+")
                         name = name.replace("CCTV5+体育赛事", "CCTV5+")
                         name = name.replace("CCTV5+体育", "CCTV5+")
-                        results.append(f"{name},{urld}")
+                        if "udp://" not in urld:
+                            results.append(f"{name},{urld}")
         except:
             continue
     except:

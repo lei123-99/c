e43ab3fe052e77130af2725e9879973eea84081b
ahua3321 <971342969@qq.com>
2025-01-09 20:42:07 +0800

    Update ipjd.py

--- ipjd.py
+++ ipjd.py
@@ -6,7 +6,6 @@ import threading
 from queue import Queue
 import eventlet
 
-
 headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'}
 
 urls = [

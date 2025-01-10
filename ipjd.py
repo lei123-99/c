9f35a2c6bb9d53ca0ce03075a93721b4efd0fba1
ahua3321 <971342969@qq.com>
2025-01-11 07:08:39 +0800

    Update ipjd.py

--- ipjd.py
+++ ipjd.py
@@ -174,8 +174,15 @@ for url in valid_urls:
     except:
         continue
         
+def channel_key(channel_name):
+    match = re.search(r'\d+', channel_name)
+    if match:
+        return int(match.group())
+    else:
+        return float('inf')  # 返回一个无穷大的数字作为关键字
+
 # 对频道进行排序
-results.sort(key=lambda x: (x[:5]))
+results.sort(key=lambda x: channel_key(x[0]))
 
 with open("iptv.txt", 'w', encoding='utf-8') as file:
     file.write('央视频道,#genre#\n')

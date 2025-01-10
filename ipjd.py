8c5f7878d277130c332a002f2427417b39602823
ahua3321 <971342969@qq.com>
2025-01-10 20:47:31 +0800

    Update ipjd.py

--- ipjd.py
+++ ipjd.py
@@ -180,8 +180,7 @@ pattern = '|'.join(keywords)  # 创建正则表达式模式，匹配任意一个
 with open("iptv.txt", 'w', encoding='utf-8') as file:
     file.write('央视频道,#genre#\n')
     for result in results:
-        channel_name, channel_url = result.split(',',1)
-        if re.search(pattern, channel_name):  # 如果行中有任意关键字
+        if re.search(pattern, result):  # 如果行中有任意关键字
             file.write(f"{channel_name},{channel_url}\n")
     file.write('卫视频道,#genre#\n')
     for result in results:

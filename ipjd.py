902d854e5b6d9a889ccc17df1cab36e53a16b8f4
ahua3321 <971342969@qq.com>
2025-01-11 19:59:26 +0800

    Update ipjd.py

--- ipjd.py
+++ ipjd.py
@@ -170,6 +170,25 @@ for url in valid_urls:
     except:
         continue
 
+channels = []
+
+for result in results:
+    line = result.strip()
+    if result:
+        channel_name, channel_url = result.split(',')
+        channels.append((channel_name, channel_url))
+       
+def channel_key(channel_name):
+    match = re.search(r'\d+', channel_name)
+    if match:
+        return int(match.group())
+    else:
+        return float('inf')  # 返回一个无穷大的数字作为关键字
+
+# 对频道进行排序
+results.sort(key=lambda x: (x[0], -float(x[2].split()[0])))
+results.sort(key=lambda x: channel_key(x[0]))
+
 result_counter = 10  # 每个频道需要的个数
 
 with open("iptv.txt", 'w', encoding='utf-8') as file:

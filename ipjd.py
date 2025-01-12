12fea95147c46ce8a0a5066527a6a6bc0a427513
ahua3321 <971342969@qq.com>
2025-01-12 10:17:23 +0800

    Update ipjd.py

--- ipjd.py
+++ ipjd.py
@@ -172,8 +172,6 @@ for url in valid_urls:
     except:
         continue
 
-results.sort(key=lambda x:int(x[4:].replace(",", "").isdigit():))
-
 with open("iptv.txt", 'w', encoding='utf-8') as file:
     file.write('央视频道,#genre#\n')
     for result in results:

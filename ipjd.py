2cb359065f8742cf85d8622466febec961f95981
ahua3321 <971342969@qq.com>
2025-01-13 16:14:03 +0800

    Update ipjd.py

--- ipjd.py
+++ ipjd.py
@@ -186,6 +186,7 @@ results = sort_by_numbers(results)
 result_counter = 8  # 每个频道需要的个数
 
 with open("iptv.txt", 'w', encoding='utf-8') as file:
+    channel_counters = {}
     file.write('央视频道,#genre#\n')
     for result in results:
         channel_name, channel_url = result.split(',',1)

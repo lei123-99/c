b0a8335e9245b1ee22f4f826bc38cf120d0921cb
ahua3321 <971342969@qq.com>
2025-01-14 08:38:18 +0800

    Update ipjd.py

--- ipjd.py
+++ ipjd.py
@@ -168,6 +168,7 @@ for url in valid_urls:
                         name = name.replace("CCTV5+体育赛视", "CCTV5+")
                         name = name.replace("CCTV5+体育赛事", "CCTV5+")
                         name = name.replace("CCTV5+体育", "CCTV5+")
+                        if "txiptv" in urld:
                         results.append(f"{name},{urld}")
         except:
             continue

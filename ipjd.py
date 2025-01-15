93a4fdb63a2301d22aa5610f1da777a559b287b9
ahua3321 <971342969@qq.com>
2025-01-15 15:17:54 +0800

    Update ipjd.py

--- ipjd.py
+++ ipjd.py
@@ -115,20 +115,20 @@ with open(rtp_filename, 'r', encoding='utf-8') as file,open(txt_filename, 'w') a
             for url in all_valid_ips:
                 new_data = data.replace("rtp://", f"http://{url}/rtp/")
                 new_file.write(new_data)
-                
-with open(rtp_filename, 'r', encoding='utf-8') as file,open(txt_filename, 'w') as new_file:
+
+with open(rtp_filename, 'r', encoding='utf-8') as file,open(txt_filename, 'a') as new_file:
     new_file.write('卫视频道,#genre#\n')
     for data in file:
         if '卫视' in data:
             for url in all_valid_ips:
                 new_data = data.replace("rtp://", f"http://{url}/rtp/")
                 new_file.write(new_data)
-                
-with open(f'df.txt', 'r', encoding='utf-8') as file,open(txt_filename, 'a') as new_file:
-    data = file.read()
-    new_file.write(data)
 
-with open(rtp_filename, 'r', encoding='utf-8') as file,open(txt_filename, 'w') as new_file:
+with open(f'df.txt', 'r', encoding='utf-8') as file,open(txt_filename, 'a') as new_file:
+        data = file.read()
+        new_file.write(data)
+
+with open(rtp_filename, 'r', encoding='utf-8') as file,open(txt_filename, 'a') as new_file:
     new_file.write('其它频道,#genre#\n')
     for data in file:
         if 'CCTV' not in data and 'CHC' not in data and '卫视' not in data:

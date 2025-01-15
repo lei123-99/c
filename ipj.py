e22db5d78b88e02e1f31f2b253cf096d16d61924
ahua3321 <971342969@qq.com>
2025-01-15 15:27:41 +0800

    Update ipj.py

--- ipj.py
+++ ipj.py
@@ -179,6 +179,11 @@ for url in valid_urls:
             continue
     except:
         continue
+        
+def natural_key(string):
+    return [int(text) if text.isdigit() else text.lower() for text in re.split(r'(\d+)', string)]
+    
+results.sort(key=lambda x: (natural_key(x[0]), -float(x[2]) if x[2] is not None else float('-inf')))
 
 with open("iptv.txt", 'w', encoding='utf-8') as file:
     file.write('央视频道,#genre#\n')

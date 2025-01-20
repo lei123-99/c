23f3ade638ac402eaea9b8c58a5e974719d3c6ac
ahua3321 <971342969@qq.com>
2025-01-20 12:21:57 +0800

    Update ipjd.py

--- ipjd.py
+++ ipjd.py
@@ -181,7 +181,7 @@ for url in valid_urls:
         continue
         
 custom_order = ['黑龙江', '吉林', '辽宁']
-results = sorted(results, key=lambda x: custom_order.index(x))
+results.sort(key=lambda x: custom_order.index(x))
 
 #def natural_key(string):
     #return [int(text) if text.isdigit() else text.lower() for text in re.split(r'(\d+)', string)]

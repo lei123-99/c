d3f3aa3f798f88570ac85400731f3541dbecd77d
ahua3321 <971342969@qq.com>
2025-01-21 15:02:41 +0800

    Update ipjd.py

--- ipjd.py
+++ ipjd.py
@@ -198,7 +198,6 @@ with open(f"d.txt", "r", encoding="utf-8") as f:
             elif current_category:
                 channel_name = line.split(",")[0].strip()
                 template_channels[current_category].append(channel_name)
-return template_channels
 
 all_channels = OrderedDict()
 for line in results:

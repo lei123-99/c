68a111c1e6f847f7e6c80953ca81add79af7574a
ahua3321 <971342969@qq.com>
2025-01-23 10:38:37 +0800

    Update ipjd.py

--- ipjd.py
+++ ipjd.py
@@ -218,7 +218,7 @@ def match_channels(template_channels, all_channels):
 def filter_source_urls(template_file):
     template_channels = parse_template(template_file)
     all_channels = OrderedDict()    
-    for category, channel_list in channels.items():
+    for category, channel_list in channels[].items():
         if category in all_channels:
             all_channels[category].extend(channel_list)
         else:

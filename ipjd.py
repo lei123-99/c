c675d4dff238fd5bab68c724be8228a51af2897d
ahua3321 <971342969@qq.com>
2025-02-05 14:39:11 +0800

    Update ipjd.py

--- ipjd.py
+++ ipjd.py
@@ -1,144 +1,174 @@
-import re
+import time
+import os
 import requests
-import logging
-from collections import OrderedDict
-from datetime import datetime
+import concurrent.futures
+import re
+import threading
+import eventlet
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', handlers=[logging.FileHandler("function.log", "w", encoding="utf-8"), logging.StreamHandler()])
+urls = [
+"http://110.183.51.1:808",
+"http://110.183.53.1:808", 
+"http://60.220.147.1:808",
+"http://183.184.104.1:8088"
+    ]
 
-def parse_template(template_file):
-    template_channels = OrderedDict()
-    current_category = None
+def modify_urls(url):
+    modified_urls = []
+    ip_start_index = url.find("//") + 2
+    ip_end_index = url.find(":", ip_start_index)
+    base_url = url[:ip_start_index]  # http:// or https://
+    ip_address = url[ip_start_index:ip_end_index]
+    port = url[ip_end_index:]
+    ip_end = "/ZHGXTV/Public/json/live_interface.txt"
+    for i in range(1, 256):
+        modified_ip = f"{ip_address[:-1]}{i}"
+        modified_url = f"{base_url}{modified_ip}{port}{ip_end}"
+        modified_urls.append(modified_url)
 
-    with open(template_file, "r", encoding="utf-8") as f:
-        for line in f:
-            line = line.strip()
-            if line and not line.startswith("#"):
-                if "#genre#" in line:
-                    current_category = line.split(",")[0].strip()
-                    template_channels[current_category] = []
-                elif current_category:
-                    channel_name = line.split(",")[0].strip()
-                    template_channels[current_category].append(channel_name)
+    return modified_urls
 
-    return template_channels
-
-def fetch_channels(url):
-    channels = OrderedDict()
-
+def is_url_accessible(url):
     try:
-        response = requests.get(url)
-        response.raise_for_status()
-        response.encoding = 'utf-8'
-        lines = response.text.split("\n")
-        current_category = None
-        is_m3u = any("#EXTINF" in line for line in lines[:15])
-        source_type = "m3u" if is_m3u else "txt"
-        logging.info(f"url: {url} 获取成功，判断为{source_type}格式")
+        response = requests.get(url, timeout=0.5)
+        if response.status_code == 200:
+            return url
+    except requests.exceptions.RequestException:
+        pass
+    return None
 
-        if is_m3u:
+results = []
+
+x_urls = []
+for url in urls:  # 对urls进行处理，ip第四位修改为1，并去重
+    url = url.strip()
+    ip_start_index = url.find("//") + 2
+    ip_end_index = url.find(":", ip_start_index)
+    ip_dot_start = url.find(".") + 1
+    ip_dot_second = url.find(".", ip_dot_start) + 1
+    ip_dot_three = url.find(".", ip_dot_second) + 1
+    base_url = url[:ip_start_index]  # http:// or https://
+    ip_address = url[ip_start_index:ip_dot_three]
+    port = url[ip_end_index:]
+    ip_end = "1"
+    modified_ip = f"{ip_address}{ip_end}"
+    x_url = f"{base_url}{modified_ip}{port}"
+    x_urls.append(x_url)
+
+urls = set(x_urls)  # 去重得到唯一的URL列表
+
+valid_urls = []
+#   多线程获取可用url
+with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
+    futures = []
+    for url in urls:
+        url = url.strip()
+        modified_urls = modify_urls(url)
+        for modified_url in modified_urls:
+            futures.append(executor.submit(is_url_accessible, modified_url))
+
+    for future in concurrent.futures.as_completed(futures):
+        result = future.result()
+        if result:
+            valid_urls.append(result)
+
+for url in valid_urls:
+    print(url)
+# 遍历网址列表，获取JSON文件并解析
+for url in valid_urls:
+    try:
+        # 发送GET请求获取JSON文件，设置超时时间为0.5秒
+        json_url = f"{url}"
+        response = requests.get(json_url, timeout=0.5)
+        json_data = response.content.decode('utf-8')
+
+        try:
+            # 按行分割数据
+            lines = json_data.split('\n')
             for line in lines:
                 line = line.strip()
-                if line.startswith("#EXTINF"):
-                    match = re.search(r'group-title="(.*?)",(.*)', line)
-                    if match:
-                        current_category = match.group(1).strip()
-                        channel_name = match.group(2).strip()
-                        if current_category not in channels:
-                            channels[current_category] = []
-                elif line and not line.startswith("#"):
-                    channel_url = line.strip()
-                    if current_category and channel_name:
-                        channels[current_category].append((channel_name, channel_url))
-        else:
-            for line in lines:
-                line = line.strip()
-                if "#genre#" in line:
-                    current_category = line.split(",")[0].strip()
-                    channels[current_category] = []
-                elif current_category:
-                    match = re.match(r"^(.*?),(.*?)$", line)
-                    if match:
-                        channel_name = match.group(1).strip()
-                        channel_url = match.group(2).strip()
-                        channels[current_category].append((channel_name, channel_url))
-                    elif line:
-                        channels[current_category].append((line, ''))
-        if channels:
-            categories = ", ".join(channels.keys())
-            logging.info(f"url: {url} 爬取成功✅，包含频道分类: {categories}")
-    except requests.RequestException as e:
-        logging.error(f"url: {url} 爬取失败❌, Error: {e}")
+                if line:
+                    name, channel_url = line.split(',',1)
+                    urls = channel_url.split('/', 3)
+                    url_data = json_url.split('/', 3)
+                    if len(urls) >= 4:
+                        urld = (f"{urls[0]}//{url_data[2]}/{urls[3]}")
+                    else:
+                        urld = (f"{urls[0]}//{url_data[2]}")
 
-    return channels
+                    if name and urld:
+                        # 删除特定文字
+                        name = name.replace("cctv", "CCTV")
+                        name = name.replace("中央", "CCTV")
+                        name = name.replace("央视", "")
+                        name = name.replace("高清", "")
+                        name = name.replace("HD", "")
+                        name = name.replace("标清", "")
+                        name = name.replace("频道", "")
+                        name = name.replace("-", "")
+                        name = name.replace(" ", "")
+                        name = name.replace("PLUS", "+")
+                        name = name.replace("＋", "+")
+                        name = name.replace("(", "")
+                        name = name.replace(")", "")
+                        name = re.sub(r"CCTV(\d+)台", r"CCTV\1", name)
+                        name = name.replace("CCTV1综合", "CCTV1")
+                        name = name.replace("CCTV2财经", "CCTV2")
+                        name = name.replace("CCTV3综艺", "CCTV3")
+                        name = name.replace("CCTV4国际", "CCTV4")
+                        name = name.replace("CCTV4中文国际", "CCTV4")
+                        name = name.replace("CCTV5体育", "CCTV5")
+                        name = name.replace("CCTV6电影", "CCTV6")
+                        name = name.replace("CCTV7军事", "CCTV7")
+                        name = name.replace("CCTV7军农", "CCTV7")
+                        name = name.replace("CCTV7农业", "CCTV7")
+                        name = name.replace("CCTV7国防军事", "CCTV7")
+                        name = name.replace("CCTV8电视剧", "CCTV8")
+                        name = name.replace("CCTV9记录", "CCTV9")
+                        name = name.replace("CCTV9纪录", "CCTV9")
+                        name = name.replace("CCTV10科教", "CCTV10")
+                        name = name.replace("CCTV11戏曲", "CCTV11")
+                        name = name.replace("CCTV12社会与法", "CCTV12")
+                        name = name.replace("CCTV13新闻", "CCTV13")
+                        name = name.replace("CCTV新闻", "CCTV13")
+                        name = name.replace("CCTV14少儿", "CCTV14")
+                        name = name.replace("CCTV少儿", "CCTV14")
+                        name = name.replace("CCTV15音乐", "CCTV15")
+                        name = name.replace("CCTV16奥林匹克", "CCTV16")
+                        name = name.replace("CCTV17农业农村", "CCTV17")
+                        name = name.replace("CCTV17农业", "CCTV17")
+                        name = name.replace("CCTV5+体育赛视", "CCTV5+")
+                        name = name.replace("CCTV5+体育赛事", "CCTV5+")
+                        name = name.replace("CCTV5+体育", "CCTV5+")
+                        if "/hls/" in urld:
+                            results.append(f"{name},{urld}")
+        except:
+            continue
+    except:
+        continue
 
-def match_channels(template_channels, all_channels):
-    matched_channels = OrderedDict()
+def natural_key(string):
+    return [int(text) if text.isdigit() else text.lower() for text in re.split(r'(\d+)', string)]
 
-    for category, channel_list in template_channels.items():
-        matched_channels[category] = OrderedDict()
-        for channel_name in channel_list:
-            for online_category, online_channel_list in all_channels.items():
-                for online_channel_name, online_channel_url in online_channel_list:
-                    if channel_name == online_channel_name:
-                        matched_channels[category].setdefault(channel_name, []).append(online_channel_url)
+results.sort(key=natural_key)
 
-    return matched_channels
+with open("iptv.txt", 'w', encoding='utf-8') as file:
+    file.write('央视频道,#genre#\n')
+    for result in results:
+        channel_name, channel_url = result.split(',')
+        if 'CCTV' in channel_name or 'CHC' in channel_name or '地理' in channel_name or '风云' in channel_name:
+            file.write(f"{channel_name},{channel_url}\n")
+    file.write('卫视频道,#genre#\n')
+    for result in results:
+        channel_name, channel_url = result.split(',')
+        if '卫视' in channel_name or '凤凰' in channel_name:
+            file.write(f"{channel_name},{channel_url}\n")
+    file.write('其他频道,#genre#\n')
+    for result in results:
+        channel_name, channel_url = result.split(',')
+        if '乐游' in channel_name or '都市' in channel_name or '车迷' in channel_name or '汽摩' in channel_name or '旅游' in channel_name:
+            file.write(f"{channel_name},{channel_url}\n")
 
-def filter_source_urls(template_file):
-    template_channels = parse_template(template_file)
-    source_urls = [
-        
-   "https://raw.bgithub.xyz/ahua3321/tvlive/refs/heads/main/iptv.txt"
-]
-
-    all_channels = OrderedDict()
-    for url in source_urls:
-        fetched_channels = fetch_channels(url)
-        for category, channel_list in fetched_channels.items():
-            if category in all_channels:
-                all_channels[category].extend(channel_list)
-            else:
-                all_channels[category] = channel_list
-
-    matched_channels = match_channels(template_channels, all_channels)
-
-    return matched_channels, template_channels
-
-def is_ipv6(url):
-    return re.match(r'^http:\/\/\[[0-9a-fA-F:]+\]', url) is not None
-
-def updateChannelUrlsM3U(channels, template_channels):
-    written_urls = set()    
-    with open("live.txt", "w", encoding="utf-8") as f_txt: 
-        for category, channel_list in template_channels.items():
-            f_txt.write(f"{category},#genre#\n")
-            if category in channels:
-                for channel_name in channel_list:
-                    if channel_name in channels[category]:                       
-                        filtered_urls = []
-                        for url in channels[category][channel_name]:
-                            filtered_urls.append(url)
-                            written_urls.add(url)
-
-                        total_urls = len(filtered_urls)
-                        for index, url in enumerate(filtered_urls, start=1):
-                            if is_ipv6(url):
-                                url_suffix = f"$LR•IPV6" if total_urls == 1 else f"$LR•IPV6『线路{index}』"
-                            else:
-                                url_suffix = f"$LR•IPV4" if total_urls == 1 else f"$LR•IPV4『线路{index}』"
-                            if '$' in url:
-                                base_url = url.split('$', 1)[0]
-                            else:
-                                base_url = url
-
-                            new_url = f"{base_url}{url_suffix}"
-
-                            f_txt.write(f"{channel_name},{new_url}\n")
-
-            f_txt.write("\n")
-
-if __name__ == "__main__":
-    template_file = "d.txt"
-    channels, template_channels = filter_source_urls(template_file)
-    updateChannelUrlsM3U(channels, template_channels)
+with open(f'df.txt', 'r', encoding='utf-8') as in_file,open(f'iptv.txt', 'a') as file:
+    data = in_file.read()
+    file.write(data)

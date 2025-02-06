fc969a775578216fb5fd91013a31a2492c2b98b7
ahua3321 <971342969@qq.com>
2025-02-06 14:54:44 +0800

    Update ipjd.py

--- ipjd.py
+++ ipjd.py
@@ -160,7 +160,7 @@ with open(rtp_filename, 'r', encoding='utf-8') as file,open(txt_filename, 'w') a
                 else:
                     for result in results:
                         channel_name, channel_url = result.split(',')
-                            if data == channel_name:
+                            if {data} == {channel_name}:
                                 new_file.write(f"{channel_name},{channel_url}\n")            
 
 with open(f'df.txt', 'r', encoding='utf-8') as file,open(txt_filename, 'a') as new_file:

cec216fd15949e950eeb0b1f75ef37d6e4ba6a1e
ahua3321 <971342969@qq.com>
2025-02-07 08:07:12 +0800

    Update ipjd.py

--- ipjd.py
+++ ipjd.py
@@ -1,168 +1,132 @@
-import time
 import os
 import requests
-import concurrent.futures
 import re
-import threading
-import eventlet
+import base64
+import cv2
+import datetime
+from datetime import datetime
 
-urls = [
-"http://110.183.51.1:808",
-"http://110.183.53.1:808",
-"http://110.183.57.1:808",
-"http://60.220.147.1:808",
-"http://183.184.104.1:8088"
-    ]
+headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36'}
 
-def modify_urls(url):
-    modified_urls = []
-    ip_start_index = url.find("//") + 2
-    ip_end_index = url.find(":", ip_start_index)
-    base_url = url[:ip_start_index]  # http:// or https://
-    ip_address = url[ip_start_index:ip_end_index]
-    port = url[ip_end_index:]
-    ip_end = "/ZHGXTV/Public/json/live_interface.txt"
-    for i in range(1, 256):
-        modified_ip = f"{ip_address[:-1]}{i}"
-        modified_url = f"{base_url}{modified_ip}{port}{ip_end}"
-        modified_urls.append(modified_url)
+# 获取rtp目录下的文件名
+files = os.listdir('rtp')
+files_name = []
 
-    return modified_urls
+# 去除后缀名并保存至provinces_isps
+for file in files:
+    name, extension = os.path.splitext(file)
+    files_name.append(name)
 
-def is_url_accessible(url):
+#忽略不符合要求的文件名
+provinces_isps = [name for name in files_name if name.count('_') == 1]
+
+# 打印结果
+print(f"本次查询：{provinces_isps}的组播节目") 
+
+keywords = []
+for province_isp in provinces_isps:
+    # 读取文件并删除空白行
     try:
-        response = requests.get(url, timeout=0.5)
-        if response.status_code == 200:
-            return url
-    except requests.exceptions.RequestException:
-        pass
-    return None
+        with open(f'rtp/{province_isp}.txt', 'r', encoding='utf-8') as file:
+            lines = file.readlines()
+            lines = [line.strip() for line in lines if line.strip()]
+        # 获取第一行中以包含 "rtp://" 的值作为 mcast
+        if lines:
+            first_line = lines[0]
+            if "rtp://" in first_line:
+                mcast = first_line.split("rtp://")[1].split(" ")[0]
+                keywords.append(province_isp + "_" + mcast)
+    except FileNotFoundError:
+    # 如果文件不存在，则捕获 FileNotFoundError 异常并打印提示信息
+        print(f"文件 '{province_isp}.txt' 不存在. 跳过此文件.")
 
-results = []
+for keyword in keywords:
+    province, isp, mcast = keyword.split("_")
+    #将省份转成英文小写
+    # 根据不同的 isp 设置不同的 org 值
+    if province == "北京" and isp == "联通":
+        isp_en = "cucc"
+        org = "China Unicom Beijing Province Network"
+    elif isp == "联通":
+        isp_en = "cucc"
+        org = "CHINA UNICOM China169 Backbone"
+    elif isp == "电信":
+        org = "Chinanet"
+        isp_en = "ctcc"
+    elif isp == "移动":
+        org == "China Mobile communications corporation"
+        isp_en = "cmcc"
 
-x_urls = []
-for url in urls:  # 对urls进行处理，ip第四位修改为1，并去重
-    url = url.strip()
-    ip_start_index = url.find("//") + 2
-    ip_end_index = url.find(":", ip_start_index)
-    ip_dot_start = url.find(".") + 1
-    ip_dot_second = url.find(".", ip_dot_start) + 1
-    ip_dot_three = url.find(".", ip_dot_second) + 1
-    base_url = url[:ip_start_index]  # http:// or https://
-    ip_address = url[ip_start_index:ip_dot_three]
-    port = url[ip_end_index:]
-    ip_end = "1"
-    modified_ip = f"{ip_address}{ip_end}"
-    x_url = f"{base_url}{modified_ip}{port}"
-    x_urls.append(x_url)
+    current_time = datetime.now()
+    result_urls = set()
 
-urls = set(x_urls)  # 去重得到唯一的URL列表
+    search_url = 'https://fofa.info/result?qbase64='
+    search_txt = f'\"udpxy\" && country=\"CN\" && region=\"{province}\" && org=\"{org}\"'
+    # 将字符串编码为字节流
+    bytes_string = search_txt.encode('utf-8')
+    # 使用 base64 进行编码
+    search_txt = base64.b64encode(bytes_string).decode('utf-8')
+    search_url += search_txt
+    print(f"{current_time} 查询运营商 : {province}{isp} ，查询网址 : {search_url}")
+    response = requests.get(search_url, headers=headers, timeout=15)
+    # 处理响应
+    response.raise_for_status()
+    # 检查请求是否成功
+    html_content = response.text
+    # 设置匹配的格式，如http://8.8.8.8:8888
+    pattern = r"http://\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d+"
+    urls_all = re.findall(pattern, html_content)
+    # 去重得到唯一的URL列表
+    result_urls = set(urls_all)
+    print(f"{current_time} result_urls:{result_urls}")
+    valid_ips = []
+    # 遍历所有视频链接
+    for url in result_urls:
+        video_url = url + "/rtp/" + mcast
+        # 用OpenCV读取视频
+        cap = cv2.VideoCapture(video_url)
+        # 检查视频是否成功打开
+        if not cap.isOpened():
+            print(f"{current_time} {video_url} 无效")
+        else:
+            # 读取视频的宽度和高度
+            width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+            height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+            print(f"{current_time} {video_url} 的分辨率为 {width}x{height}")
+            # 检查分辨率是否大于0
+            if width > 0 and height > 0:
+                valid_ips.append(url)
+            # 关闭视频流
+            cap.release()
 
-valid_urls = []
-#   多线程获取可用url
-with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
-    futures = []
-    for url in urls:
-        url = url.strip()
-        modified_urls = modify_urls(url)
-        for modified_url in modified_urls:
-            futures.append(executor.submit(is_url_accessible, modified_url))
-
-    for future in concurrent.futures.as_completed(futures):
-        result = future.result()
-        if result:
-            valid_urls.append(result)
-
-for url in valid_urls:
-    print(url)
-# 遍历网址列表，获取JSON文件并解析
-for url in valid_urls:
-    try:
-        # 发送GET请求获取JSON文件，设置超时时间为0.5秒
-        json_url = f"{url}"
-        response = requests.get(json_url, timeout=0.5)
-        json_data = response.content.decode('utf-8')
-
-        try:
-            # 按行分割数据
-            lines = json_data.split('\n')
-            for line in lines:
-                line = line.strip()
-                if line:
-                    name, channel_url = line.split(',',1)
-                    urls = channel_url.split('/', 3)
-                    url_data = json_url.split('/', 3)
-                    if len(urls) >= 4:
-                        urld = (f"{urls[0]}//{url_data[2]}/{urls[3]}")
-                    else:
-                        urld = (f"{urls[0]}//{url_data[2]}")
-
-                    if name and urld:
-                        # 删除特定文字
-                        name = name.replace("cctv", "CCTV")
-                        name = name.replace("中央", "CCTV")
-                        name = name.replace("央视", "")
-                        name = name.replace("高清", "")
-                        name = name.replace("HD", "")
-                        name = name.replace("标清", "")
-                        name = name.replace("频道", "")
-                        name = name.replace("-", "")
-                        name = name.replace(" ", "")
-                        name = name.replace("PLUS", "+")
-                        name = name.replace("＋", "+")
-                        name = name.replace("(", "")
-                        name = name.replace(")", "")
-                        name = re.sub(r"CCTV(\d+)台", r"CCTV\1", name)
-                        name = name.replace("CCTV1综合", "CCTV1")
-                        name = name.replace("CCTV2财经", "CCTV2")
-                        name = name.replace("CCTV3综艺", "CCTV3")
-                        name = name.replace("CCTV4国际", "CCTV4")
-                        name = name.replace("CCTV4中文国际", "CCTV4")
-                        name = name.replace("CCTV5体育", "CCTV5")
-                        name = name.replace("CCTV6电影", "CCTV6")
-                        name = name.replace("CCTV7军事", "CCTV7")
-                        name = name.replace("CCTV7军农", "CCTV7")
-                        name = name.replace("CCTV7农业", "CCTV7")
-                        name = name.replace("CCTV7国防军事", "CCTV7")
-                        name = name.replace("CCTV8电视剧", "CCTV8")
-                        name = name.replace("CCTV9记录", "CCTV9")
-                        name = name.replace("CCTV9纪录", "CCTV9")
-                        name = name.replace("CCTV10科教", "CCTV10")
-                        name = name.replace("CCTV11戏曲", "CCTV11")
-                        name = name.replace("CCTV12社会与法", "CCTV12")
-                        name = name.replace("CCTV13新闻", "CCTV13")
-                        name = name.replace("CCTV新闻", "CCTV13")
-                        name = name.replace("CCTV14少儿", "CCTV14")
-                        name = name.replace("CCTV少儿", "CCTV14")
-                        name = name.replace("CCTV15音乐", "CCTV15")
-                        name = name.replace("CCTV16奥林匹克", "CCTV16")
-                        name = name.replace("CCTV17农业农村", "CCTV17")
-                        name = name.replace("CCTV17农业", "CCTV17")
-                        name = name.replace("CCTV5+体育赛视", "CCTV5+")
-                        name = name.replace("CCTV5+体育赛事", "CCTV5+")
-                        name = name.replace("CCTV5+体育", "CCTV5+")
-                        if "udp://" not in urld:
-                            results.append(f"{name},{urld}")
-        except:
-            continue
-    except:
-        continue
-
-rtp_filename = f'mb.txt'
+#生成节目列表 省份运营商.txt
+rtp_filename = f'rtp/{province}_{isp}.txt'
 txt_filename = f'iptv.txt'
-
 with open(rtp_filename, 'r', encoding='utf-8') as file,open(txt_filename, 'w') as new_file:
+    new_file.write('央视频道,#genre#\n')
     for data in file:
-        data = data.strip()
-        if data and not data.startswith("#"):
-            if "#genre#" in data:
-                new_file.write(data + '\n')
-            else:
-                for result in results:
-                    channel_name, channel_url = result.split(',')
-                    if {data} == {channel_name}:
-                        new_file.write(f"{channel_name},{channel_url}\n")
+        if 'CCTV' in data or 'CHC' in data:
+            for url in valid_ips:
+                new_data = data.replace("rtp://", f"{url}/rtp/")
+                new_file.write(new_data)
+
+with open(rtp_filename, 'r', encoding='utf-8') as file,open(txt_filename, 'a') as new_file:
+    new_file.write('卫视频道,#genre#\n')
+    for data in file:
+        if '卫视' in data:
+            for url in valid_ips:
+                new_data = data.replace("rtp://", f"{url}/rtp/")
+                new_file.write(new_data)
 
 with open(f'df.txt', 'r', encoding='utf-8') as file,open(txt_filename, 'a') as new_file:
     data = file.read()
     new_file.write(data)
+
+with open(rtp_filename, 'r', encoding='utf-8') as file,open(txt_filename, 'a') as new_file:
+    new_file.write('其它频道,#genre#\n')
+    for data in file:
+        if 'CCTV' not in data and 'CHC' not in data and '卫视' not in data:
+            for url in valid_ips:
+                new_data = data.replace("rtp://", f"{url}/rtp/")
+                new_file.write(new_data)
+print(f'已生成播放列表，保存至{txt_filename}')

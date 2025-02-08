b5326e0b36028d17ce109b100d604f973bd72bb3
ahua3321 <971342969@qq.com>
2025-02-08 08:15:29 +0800

    Update ipjd.py

--- ipjd.py
+++ ipjd.py
@@ -102,7 +102,7 @@ for url in valid_urls:
                     else:
                         urld = f"{url_x}{urlx}"
 
-                    if name and urlx:
+                    if name and urld:
                         # 删除特定文字
                         name = name.replace("cctv", "CCTV")
                         name = name.replace("中央", "CCTV")

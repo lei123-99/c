1656dabc5608cac86ee7bd455b77354a3761fefb
ahua3321 <971342969@qq.com>
2025-02-09 07:14:58 +0800

    Update ipjd1.py

--- ipjd1.py
+++ ipjd1.py
@@ -28,7 +28,7 @@ def modify_urls(url):
 
 def is_url_accessible(url):
     try:
-        response = requests.get(url, timeout=15)
+        response = requests.get(url, timeout=0.5)
         if response.status_code == 200:
             return url
     except requests.exceptions.RequestException:
@@ -77,7 +77,7 @@ for url in valid_urls:
     try:
         # 发送GET请求获取JSON文件，设置超时时间为0.5秒        
         json_url = f'{url}'
-        response = requests.get(json_url, timeout=1)
+        response = requests.get(json_url, timeout=0.5)
         json_data = response.content.decode('utf-8')
 
         try:

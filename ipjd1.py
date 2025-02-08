ad1c4bf90e936eac7d8b0f0061d6c8e86c960ca8
ahua3321 <971342969@qq.com>
2025-02-08 13:18:21 +0800

    Update ipjd1.py

--- ipjd1.py
+++ ipjd1.py
@@ -1,111 +1,147 @@
 import time
 import os
 import requests
+import concurrent.futures
 import re
 import threading
-from queue import Queue
 import eventlet
 
-
-headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'}
-
 urls = [
-    "https://fofa.info/result?qbase64=IlpIR1hUViIgJiYgY2l0eT0ic2hpamlhemh1YW5nIg%3D%3D",  #shijiazhuang 石家庄
-    "https://fofa.info/result?qbase64=IlpIR1hUViIgJiYgY2l0eT0iaGFuZGFuIg%3D%3D",  # handan 邯郸
-    "https://fofa.info/result?qbase64=IlpIR1hUViIgJiYgY2l0eT0iYmFvZGluZyI%3D",  # baoding 保定
-    "https://fofa.info/result?qbase64=IlpIR1hUViIgJiYgY2l0eT0idGFuZ3NoYW4i",  # tangshan 唐山
-    "https://fofa.info/result?qbase64=IlpIR1hUViIgJiYgY2l0eT0iaGVuZ3NodWki",  # henghsui 衡水
-   # "https://fofa.info/result?qbase64=IlpIR1hUViIgJiYgY2l0eT0ieGluZ3RhaSI%3D",  # xingtai 邢台
-    "https://fofa.info/result?qbase64=IlpIR1hUViIgJiYgY2l0eT0iemhhbmdqaWFrb3Ui",  # zhangjiakou 张家口
-    "https://fofa.info/result?qbase64=IlpIR1hUViIgJiYgY2l0eT0iY2FuZ3pob3Ui",  # cangzhou 沧州
-    "https://fofa.info/result?qbase64=IlpIR1hUViIgJiYgY2l0eT0ibGFuZ2Zhbmci",  # langfang 廊坊
+"http://110.183.49.1:808",
+"http://60.220.147.1:808"
+    ]
 
-]
+def modify_urls(url):
+    modified_urls = []
+    ip_start_index = url.find("//") + 2
+    ip_end_index = url.find(":", ip_start_index)
+    base_url = url[:ip_start_index]  # http:// or https://
+    ip_address = url[ip_start_index:ip_end_index]
+    port = url[ip_end_index:]
+    ip_end = "/ZHGXTV/Public/json/live_interface.txt"
+    for i in range(1, 256):
+        modified_ip = f"{ip_address[:-1]}{i}"
+        modified_url = f"{base_url}{modified_ip}{port}{ip_end}"
+        modified_urls.append(modified_url)
+
+    return modified_urls
+
+def is_url_accessible(url):
+    try:
+        response = requests.get(url, timeout=15)
+        if response.status_code == 200:
+            return url
+    except requests.exceptions.RequestException:
+        pass
+    return None
 
 results = []
 
-for url in urls:
+x_urls = []
+for url in urls:  # 对urls进行处理，ip第四位修改为1，并去重
+    url = url.strip()
+    ip_start_index = url.find("//") + 2
+    ip_end_index = url.find(":", ip_start_index)
+    ip_dot_start = url.find(".") + 1
+    ip_dot_second = url.find(".", ip_dot_start) + 1
+    ip_dot_three = url.find(".", ip_dot_second) + 1
+    base_url = url[:ip_start_index]  # http:// or https://
+    ip_address = url[ip_start_index:ip_dot_three]
+    port = url[ip_end_index:]
+    ip_end = "1"
+    modified_ip = f"{ip_address}{ip_end}"
+    x_url = f"{base_url}{modified_ip}{port}"
+    x_urls.append(x_url)
+
+urls = set(x_urls)  # 去重得到唯一的URL列表
+
+valid_urls = []
+#   多线程获取可用url
+with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
+    futures = []
+    for url in urls:
+        url = url.strip()
+        modified_urls = modify_urls(url)
+        for modified_url in modified_urls:
+            futures.append(executor.submit(is_url_accessible, modified_url))
+
+    for future in concurrent.futures.as_completed(futures):
+        result = future.result()
+        if result:
+            valid_urls.append(result)
+
+for url in valid_urls:
+    print(url)
+# 遍历网址列表，获取JSON文件并解析
+for url in valid_urls:
     try:
-        response = requests.get(url, headers=headers, timeout=15)
-        page_content = response.text
-        # 查找所有符合指定格式的网址
-        pattern = r"http://\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d+"  # 设置匹配的格式，如http://8.8.8.8:8888
-        urls_all = re.findall(pattern, page_content)
-        # urls = list(set(urls_all))  # 去重得到唯一的URL列表
-        urls = set(urls_all)  # 去重得到唯一的URL列表
-        # 遍历网址列表，获取JSON文件并解析
-        for url in urls:
-            try:
-                # 发送GET请求获取JSON文件，设置超时时间为0.5秒
-                json_url = f'{url}/ZHGXTV/Public/json/live_interface.txt'
-                response = requests.get(json_url, timeout=5)
-                json_data = response.content.decode('utf-8')
-                try:
-                    # 按行分割数据
-                    lines = json_data.split('\n')
-                    for line in lines:
-                        line = line.strip()
-                        if line:
-                            name, channel_url = line.split(',',1)
-                            urls = channel_url.split('/', 3)
-                            url_data = json_url.split('/', 3)
-                            if len(urls) >= 4:
-                                urld = (f"{urls[0]}//{url_data[2]}/{urls[3]}")
-                            else:
-                                urld = (f"{urls[0]}//{url_data[2]}")
-                            if name and urld:
-                                # 删除特定文字
-                                name = name.replace("cctv", "CCTV")
-                                name = name.replace("中央", "CCTV")
-                                name = name.replace("央视", "CCTV")
-                                name = name.replace("高清", "")
-                                name = name.replace("超高", "")
-                                name = name.replace("HD", "")
-                                name = name.replace("标清", "")
-                                name = name.replace("频道", "")
-                                name = name.replace("-", "")
-                                name = name.replace(" ", "")
-                                name = name.replace("PLUS", "+")
-                                name = name.replace("＋", "+")
-                                name = name.replace("(", "")
-                                name = name.replace(")", "")
-                                name = re.sub(r"CCTV(\d+)台", r"CCTV\1", name)
-                                name = name.replace("CCTV1综合", "CCTV1")
-                                name = name.replace("CCTV2财经", "CCTV2")
-                                name = name.replace("CCTV3综艺", "CCTV3")
-                                name = name.replace("CCTV4国际", "CCTV4")
-                                name = name.replace("CCTV4中文国际", "CCTV4")
-                                name = name.replace("CCTV4欧洲", "CCTV4")
-                                name = name.replace("CCTV5体育", "CCTV5")
-                                name = name.replace("CCTV6电影", "CCTV6")
-                                name = name.replace("CCTV7军事", "CCTV7")
-                                name = name.replace("CCTV7军农", "CCTV7")
-                                name = name.replace("CCTV7农业", "CCTV7")
-                                name = name.replace("CCTV7国防军事", "CCTV7")
-                                name = name.replace("CCTV8电视剧", "CCTV8")
-                                name = name.replace("CCTV9记录", "CCTV9")
-                                name = name.replace("CCTV9纪录", "CCTV9")
-                                name = name.replace("CCTV10科教", "CCTV10")
-                                name = name.replace("CCTV11戏曲", "CCTV11")
-                                name = name.replace("CCTV12社会与法", "CCTV12")
-                                name = name.replace("CCTV12法制", "CCTV12")
-                                name = name.replace("CCTV13新闻", "CCTV13")
-                                name = name.replace("CCTV新闻", "CCTV13")
-                                name = name.replace("CCTV14少儿", "CCTV14")
-                                name = name.replace("CCTV15音乐", "CCTV15")
-                                name = name.replace("CCTV16奥林匹克", "CCTV16")
-                                name = name.replace("CCTV17农业农村", "CCTV17")
-                                name = name.replace("CCTV17农业", "CCTV17")
-                                name = name.replace("CCTV5+体育赛视", "CCTV5+")
-                                name = name.replace("CCTV5+体育赛事", "CCTV5+")
-                                name = name.replace("CCTV5+体育", "CCTV5+")
-                                name = name.replace("河北少儿科教", "河北少儿")
-                                if "udp://" not in urld:
-                                    results.append(f"{name},{urld}")
-                except:
-                    continue
-            except:
-                continue
+        # 发送GET请求获取JSON文件，设置超时时间为0.5秒        
+        json_url = f'{url}'
+        response = requests.get(json_url, timeout=0.5)
+        json_data = response.content.decode('utf-8')
+
+        try:
+            # 按行分割数据
+            lines = json_data.split('\n')
+            for line in lines:
+                line = line.strip()
+                if line:
+                    name, channel_url = line.split(',',1)
+                    urls = channel_url.split('/', 3)
+                    url_data = json_url.split('/', 3)
+                    if len(urls) >= 4:
+                        urld = (f"{urls[0]}//{url_data[2]}/{urls[3]}")
+                    else:
+                        urld = (f"{urls[0]}//{url_data[2]}")
+
+                    if name and urld:
+                        # 删除特定文字
+                        name = name.replace("cctv", "CCTV")
+                        name = name.replace("中央", "CCTV")
+                        name = name.replace("央视", "")
+                        name = name.replace("高清", "")
+                        name = name.replace("HD", "")
+                        name = name.replace("标清", "")
+                        name = name.replace("频道", "")
+                        name = name.replace("-", "")
+                        name = name.replace(" ", "")
+                        name = name.replace("PLUS", "+")
+                        name = name.replace("＋", "+")
+                        name = name.replace("(", "")
+                        name = name.replace(")", "")
+                        name = re.sub(r"CCTV(\d+)台", r"CCTV\1", name)
+                        name = name.replace("CCTV1综合", "CCTV1")
+                        name = name.replace("CCTV2财经", "CCTV2")
+                        name = name.replace("CCTV3综艺", "CCTV3")
+                        name = name.replace("CCTV4国际", "CCTV4")
+                        name = name.replace("CCTV4中文国际", "CCTV4")
+                        name = name.replace("CCTV5体育", "CCTV5")
+                        name = name.replace("CCTV6电影", "CCTV6")
+                        name = name.replace("CCTV7军事", "CCTV7")
+                        name = name.replace("CCTV7军农", "CCTV7")
+                        name = name.replace("CCTV7农业", "CCTV7")
+                        name = name.replace("CCTV7国防军事", "CCTV7")
+                        name = name.replace("CCTV8电视剧", "CCTV8")
+                        name = name.replace("CCTV9记录", "CCTV9")
+                        name = name.replace("CCTV9纪录", "CCTV9")
+                        name = name.replace("CCTV10科教", "CCTV10")
+                        name = name.replace("CCTV11戏曲", "CCTV11")
+                        name = name.replace("CCTV12社会与法", "CCTV12")
+                        name = name.replace("CCTV13新闻", "CCTV13")
+                        name = name.replace("CCTV新闻", "CCTV13")
+                        name = name.replace("CCTV14少儿", "CCTV14")
+                        name = name.replace("CCTV少儿", "CCTV14")
+                        name = name.replace("CCTV15音乐", "CCTV15")
+                        name = name.replace("CCTV16奥林匹克", "CCTV16")
+                        name = name.replace("CCTV17农业农村", "CCTV17")
+                        name = name.replace("CCTV17农业", "CCTV17")
+                        name = name.replace("CCTV5+体育赛视", "CCTV5+")
+                        name = name.replace("CCTV5+体育赛事", "CCTV5+")
+                        name = name.replace("CCTV5+体育", "CCTV5+")
+                        if "/hls/" in urld:
+                            results.append(f"{name},{urld}")
+        except:
+            continue
     except:
         continue
 

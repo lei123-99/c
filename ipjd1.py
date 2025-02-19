715d213b7f6ddad6ebbb200d5bdac284078ffe54
lei123-99 <syxingjunyi@163.com>
2025-02-19 16:02:36 +0800

    Update ipjd1.py

--- ipjd1.py
+++ ipjd1.py
@@ -3,12 +3,14 @@ import os
 import requests
 import concurrent.futures
 import re
+import threading
+import eventlet
 
 urls = [
-    "http://60.220.147.1:808",
-    "http://183.191.0.1:9003",
-    "http://110.183.49.1:808"
-]
+"http://60.220.147.1:808",
+"http://183.191.0.1:9003",
+"http://110.183.49.1:808"
+    ]
 
 def modify_urls(url):
     modified_urls = []
@@ -37,14 +39,14 @@ def is_url_accessible(url):
 results = []
 
 x_urls = []
-for url in urls:
+for url in urls:  # 对urls进行处理，ip第四位修改为1，并去重
     url = url.strip()
     ip_start_index = url.find("//") + 2
     ip_end_index = url.find(":", ip_start_index)
     ip_dot_start = url.find(".") + 1
     ip_dot_second = url.find(".", ip_dot_start) + 1
     ip_dot_three = url.find(".", ip_dot_second) + 1
-    base_url = url[:ip_start_index]
+    base_url = url[:ip_start_index]  # http:// or https://
     ip_address = url[ip_start_index:ip_dot_three]
     port = url[ip_end_index:]
     ip_end = "1"
@@ -52,9 +54,10 @@ for url in urls:
     x_url = f"{base_url}{modified_ip}{port}"
     x_urls.append(x_url)
 
-urls = set(x_urls)
+urls = set(x_urls)  # 去重得到唯一的URL列表
 
 valid_urls = []
+#   多线程获取可用url
 with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
     futures = []
     for url in urls:
@@ -69,54 +72,95 @@ with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
             valid_urls.append(result)
 
 for url in valid_urls:
+    print(url)
+# 遍历网址列表，获取JSON文件并解析
+for url in valid_urls:
     try:
-        response = requests.get(url, timeout=0.5)
+        # 发送GET请求获取JSON文件，设置超时时间为0.5秒
+        json_url = f"{url}"
+        response = requests.get(json_url, timeout=0.5)
         json_data = response.content.decode('utf-8')
 
-        lines = json_data.split('\n')
-        for line in lines:
-            line = line.strip()
-            if line:
-                name, channel_url = line.split(',', 1)
-                urls = channel_url.split('/', 3)
-                url_data = json_url.split('/', 3)
-                if len(urls) >= 4:
-                    urld = f"{urls[0]}//{url_data[2]}/{urls[3]}"
-                else:
-                    urld = f"{urls[0]}//{url_data[2]}"
+        try:
+            # 按行分割数据
+            lines = json_data.split('\n')
+            for line in lines:
+                line = line.strip()
+                if line:
+                    name, channel_url = line.split(',',1)
+                    urls = channel_url.split('/', 3)
+                    url_data = json_url.split('/', 3)
+                    if len(urls) >= 4:
+                        urld = (f"{urls[0]}//{url_data[2]}/{urls[3]}")
+                    else:
+                        urld = (f"{urls[0]}//{url_data[2]}")
 
-                if name and urld:
-                    name = re.sub(r"CCTV(\d+)台", r"CCTV\1", name)
-                    # 这里可以添加更多的名称替换逻辑
-                    if "/hls/" in urld:
-                        results.append(f"{name},{urld}")
-    except Exception as e:
-        print(f"Error processing URL {url}: {e}")
+                    if name and urld:
+                        # 删除特定文字
+                        name = name.replace("cctv", "CCTV")
+                        name = name.replace("中央", "CCTV")
+                        name = name.replace("央视", "")
+                        name = name.replace("高清", "")
+                        name = name.replace("HD", "")
+                        name = name.replace("标清", "")
+                        name = name.replace("频道", "")
+                        name = name.replace("-", "")
+                        name = name.replace(" ", "")
+                        name = name.replace("PLUS", "+")
+                        name = name.replace("＋", "+")
+                        name = name.replace("(", "")
+                        name = name.replace(")", "")
+                        name = re.sub(r"CCTV(\d+)台", r"CCTV\1", name)
+                        name = name.replace("CCTV1综合", "CCTV1")
+                        name = name.replace("CCTV2财经", "CCTV2")
+                        name = name.replace("CCTV3综艺", "CCTV3")
+                        name = name.replace("CCTV4国际", "CCTV4")
+                        name = name.replace("CCTV4中文国际", "CCTV4")
+                        name = name.replace("CCTV5体育", "CCTV5")
+                        name = name.replace("CCTV6电影", "CCTV6")
+                        name = name.replace("CCTV7军事", "CCTV7")
+                        name = name.replace("CCTV7军农", "CCTV7")
+                        name = name.replace("CCTV7农业", "CCTV7")
+                        name = name.replace("CCTV7国防军事", "CCTV7")
+                        name = name.replace("CCTV8电视剧", "CCTV8")
+                        name = name.replace("CCTV9记录", "CCTV9")
+                        name = name.replace("CCTV9纪录", "CCTV9")
+                        name = name.replace("CCTV10科教", "CCTV10")
+                        name = name.replace("CCTV11戏曲", "CCTV11")
+                        name = name.replace("CCTV12社会与法", "CCTV12")
+                        name = name.replace("CCTV13新闻", "CCTV13")
+                        name = name.replace("CCTV新闻", "CCTV13")
+                        name = name.replace("CCTV14少儿", "CCTV14")
+                        name = name.replace("CCTV少儿", "CCTV14")
+                        name = name.replace("CCTV15音乐", "CCTV15")
+                        name = name.replace("CCTV16奥林匹克", "CCTV16")
+                        name = name.replace("CCTV17农业农村", "CCTV17")
+                        name = name.replace("CCTV17农业", "CCTV17")
+                        name = name.replace("CCTV5+体育赛视", "CCTV5+")
+                        name = name.replace("CCTV5+体育赛事", "CCTV5+")
+                        name = name.replace("CCTV5+体育", "CCTV5+")
+                        if "/hls/" in urld:
+                            results.append(f"{name},{urld}")
+        except:
+            continue
+    except:
+        continue
 
-rtp_filename = 'mb.txt'
-txt_filename = 'itv.txt'
+rtp_filename = f'mb.txt'
+txt_filename = f'itv.txt'
 
-# 确保 mb.txt 文件存在
-if not os.path.exists(rtp_filename):
-    print(f"{rtp_filename} 文件不存在，请检查。")
-else:
-    with open(rtp_filename, 'r', encoding='utf-8') as file, open(txt_filename, 'w') as new_file:
-        for data in file:
-            data = data.strip()
-            if data and not data.startswith("#"):
-                if "#genre#" in data:
-                    new_file.write(data + '\n')
-                else:
-                    for result in results:
-                        channel_name, channel_url = result.split(',')
-                        if channel_name == data:
-                            new_file.write(f"{channel_name},{channel_url}\n")
+with open(rtp_filename, 'r', encoding='utf-8') as file,open(txt_filename, 'w') as new_file:
+    for data in file:
+        data = data.strip()
+        if data and not data.startswith("#"):
+            if "#genre#" in data:
+                new_file.write(data + '\n')
+            else:
+                for result in results:
+                    channel_name, channel_url = result.split(',')
+                    if {data} == {channel_name}:
+                        new_file.write(f"{channel_name},{channel_url}\n")
 
-    # 追加 df.txt 的内容
-    df_filename = 'df.txt'
-    if os.path.exists(df_filename):
-        with open(df_filename, 'r', encoding='utf-8') as file, open(txt_filename, 'a') as new_file:
-            data = file.read()
-            new_file.write(data)
-    else:
-        print(f"{df_filename} 文件不存在，请检查。")
+with open(f'df.txt', 'r', encoding='utf-8') as file,open(txt_filename, 'a') as new_file:
+    data = file.read()
+    new_file.write(data)
